2b5bc3f3234ffb82c192f8ca184c4523feea767f
Kyle <kyle.stephens@ucdenver.edu>
2023-06-23 14:38:28 -0600

    made app.py template and adjusted table names

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #import flask
 from flask import Flask ,  render_template
-app = Flask(__name__)
+# import psycopg2
+
+app = Flask(__name__,template_folder='template')#you can add the folder with the static HTML, CSS and Javascript files here
 
 
 #making the first app route use template in two lines below if you want more pages
@@ -9,7 +11,10 @@ app = Flask(__name__)
 # def hello():
 @app.route('/')
 def hello():
-    return render_template('home.html')
+    return render_template('home.html')#put the html file of your home page here
 
 if __name__ == '__main__':
     app.run(debug=True) #turn off debug if it goes into production? i see it mentioned IDK what that means
+
+
+#to run the flask server type 'python app.py' into bash to close the server hit 'ctrl+c'
