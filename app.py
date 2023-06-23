6c7fefe1a12afc9d800becaf0bafe1f58bb432f7
Kyle <kyle.stephens@ucdenver.edu>
2023-06-23 13:50:51 -0600

    adding home.html to demo flask app

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-#Flask app
-from flask import Flask
+#import flask
+from flask import Flask ,  render_template
 app = Flask(__name__)
 
-@app.route('/dashboard')
-def hello():
-    return 'Hello World!'
 
-@app.route('/about')
+#making the first app route use template in two lines below if you want more pages
+    #in the '@app.route line you have to chose the page url in the ('/') example: ('/dashboard') 
+# @app.route('/')
+# def hello():
+@app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) #turn off debug if it goes into production? i see it mentioned IDK what that means
