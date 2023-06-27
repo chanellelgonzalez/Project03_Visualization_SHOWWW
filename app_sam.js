5425c0ef89e1ced1978c91888dccc8f4167cd0c5
Sam Spillane <Samspillane@gmail.com>
2023-06-26 18:12:50 -0700

    Added select and deselect all and a spiderfier.

--- app_sam.js
+++ app_sam.js
@@ -1,33 +1,21 @@
 /*
-NEW CHANGES: In all instances with the layer group I added an "Other" layer so events that were caterogized outside of the regular layers can still be displayed.
-Those changes are in "layers","map","overlays", "icons", and "eventCount".
-----------------------------------------------------------------------------
-The style.css file has been updated so make sure we are working with the new one
-----------------------------------------------------------------------------
-The .html file will have to be updated with the new Awesome Markers Icons:
-NOTE: This is funky but it works
- ---------------------------------------------------------------------------
-<!-- Awesome Markers Icons -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Leaflet.awesome-markers/2.0.2/leaflet.awesome-markers.css">
-  <link rel="stylesheet" href="css/leaflet.awesome-markers.css">
-  <script src="js/leaflet.awesome-markers.js"></script>
-  
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Leaflet.awesome-markers/2.0.2/leaflet.awesome-markers.css">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Leaflet.awesome-markers/2.0.2/leaflet.awesome-markers.js"></script>
- ---------------------------------------------------------------------------
+NEW CHANGES:
+add to html head
 
- Also grab "leaflet.awesome-markers.js" from js and "leaflet.awesome-markers.css" from css
+ <!-- Overlapping Marker Spiderfier -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OverlappingMarkerSpiderfier-Leaflet/0.2.6/oms.min.js"></script>
 */
 
-// Create the tile layer with leaflet (If there is more time look into how to change the visual representation of the map)
+//Create the tile layer with leaflet (If there is more time look into how to change the visual representation of the map)
 let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
-
-// Initialize all the LayerGroups (i.e. genre groups)
-// Note there are a few genres that are unlikley to have any events and I have determined should be skipped such as:
-// Chanson Francaise, Children's Music,Holiday,Medieval/Renaissance, Other, Undefined.
+/*
+Initialize all the LayerGroups (i.e. genre groups).
+Note there are a few genres that are unlikley to have any events and I have determined should be merged under a "other"
+category such as: Chanson Francaise, Children's Music,Holiday,Medieval/Renaissance, Other, Undefined.
+*/
 let layers = {
     Alternative: new L.LayerGroup(),
     Ballads_or_Romantic: new L.LayerGroup(),
@@ -49,14 +37,16 @@ let layers = {
 };
 // There are 17 layers for the genres
 
-// Create and add the layers to the map
-// This is also where we center the map and set the zoom level (right now the view covers Oxnard to San Bernardino going to keep it here)
-// note that how we connect to HTML will be through "map-id" I kept it the same as we use in class.
+/*
+Create and add the layers to the map
+This is also where we center the map and set the zoom level (right now the view covers Oxnard to San Bernardino going to keep it here).
+Note that how we connect to HTML will be through "map-id" I kept it the same as we use in class.
+*/
 let mapCenter = []
 
 let map = L.map("map-id", {
     center: [34.0522, -118.2437],
-    zoom: 10,
+    zoom: 8,
     layers: [
     layers.Alternative,
     layers.Ballads_or_Romantic,
@@ -81,14 +71,31 @@ let map = L.map("map-id", {
 // Add our "streetmap" tile layer to the map.
 streetmap.addTo(map);
 
-
 /*----------------------------------------------------------------------
-Over lapping Marker Spiderfier code
+NEW CHANGES: Overlapping Marker Spiderfier code
 ----------------------------------------------------------------------*/ 
+// This allows the user to click on overlapping markers and it spreads them out.
 
 // Create a layer for the Over lapping Marker Spiderfier
-// This allows the user to click on overlapping markers and it spreads them out
-// var oms = new OverlappingMarkerSpiderfier(map);
+var oms = new OverlappingMarkerSpiderfier(map);
+
+// Create new popup for the oms
+var popup = new L.Popup();
+
+oms.addListener('click', function(marker) {
+  popup.setContent(marker.desc);
+  popup.setLatLng(marker.getLatLng());
+  //map.openPopup(popup);
+  map.closePopup(popup);
+});
+
+oms.addListener('spiderfy', function(markers) {
+
+    Newmarker.bindPopup('');
+        
+    map.closePopup();
+});
+//---------------------------------------------------------------------
 
 // Create an overlays object to add to the layer control.
 let overlays = {
@@ -111,14 +118,48 @@ let overlays = {
     "Other Genres": layers.Other
   };
 
-// Create a control for our layers, and add our overlays to it. (This allows us to select which layers we want to see)
-//Get rid of this when we filter from the dropdown menu
-L.control.layers(null, overlays).addTo(map);
+/*------------------------------------------------------------
+NEW CHANGES: Added select all and deselect all buttons to the map
+------------------------------------------------------------*/
+// Save L.control to the variable controlPanel
+let controlPanel = L.control.layers(null, overlays).addTo(map);
+
+// Add the "Select All" button to the layers control
+var selectAll = L.DomUtil.create('button', 'select-all-button');
+selectAll.innerHTML = 'Select All';
+// add a event listener that checks if any layer is not selected and then selects it
+selectAll.addEventListener('click', function() {
+  Object.values(controlPanel._layers).forEach(function(layer) {
+    if (!map.hasLayer(layer.layer)) {
+      map.addLayer(layer.layer);
+    }
+  });
+});
+
+// Add the "Deselect All" button to the layers control
+var deselectAll = L.DomUtil.create('button', 'deselect-all-button');
+deselectAll.innerHTML = 'Deselect All';
+// add a event listener that checks if any layer is selected and then deselects it
+deselectAll.addEventListener('click', function() {
+  Object.values(controlPanel._layers).forEach(function(layer) {
+    if (map.hasLayer(layer.layer)) {
+      map.removeLayer(layer.layer);
+    }
+  });
+});
+
+// Create a container for the button and append selectAll and deselectAll under it.
+var Container = L.DomUtil.create('div', 'buttons-container');
+Container.appendChild(selectAll);
+Container.appendChild(deselectAll);
+
+// Add the container to the layers control
+controlPanel.getContainer().appendChild(Container);
 
 
-// Legends for the map Might not be needed (doesn't look amazing anyway)
-//-------------------------
-// Create a legend to display information about our map.
+/*---------------------------------------------------
+Create a legend to display information about our map.
+----------------------------------------------------*/
 let info = L.control({
     position: "bottomright"
   });
@@ -130,19 +171,13 @@ info.onAdd = function() {
   };
 // Add the info legend to the map.
 info.addTo(map);
-//-------------------------
 
 
 
 /* ----------------------------------------------------------------------
-Initialize a dictionary that contains icons for each layer group. use shape:
-"star" for outside shape and icon: "ion-settings" for the inside icon. 
-Come back to these later to pick colors but for now just make them all different.
+Initialize a dictionary that contains icons for each layer group. 
 ---------------------------------------------------------------------- */
 
-/*------------------------------------------
-NEW CHANGES: Delete old icons dictionary and add the stuff below
-------------------------------------------*/
 //Define colors in a var
 var icon_colors = ['red', 'blue', 'green', 'purple', 'orange', 'darkred', 'lightred', 'beige', 'darkblue', 'darkgreen', 'cadetblue', 'darkpurple', 'white', 'pink', 'lightblue', 'lightgreen', 'gray', 'black', 'lightgray'];
 
@@ -183,8 +218,9 @@ let icons = {
     R_and_B: L.AwesomeMarkers.icon({
         markerColor: icon_colors[11],
     }),
+    //NEW CHANGES: reggae color is changed here and in the style.css file (lightgray)
     Reggae: L.AwesomeMarkers.icon({
-        markerColor: icon_colors[12],
+        markerColor: icon_colors[18],
     }),
     Religious: L.AwesomeMarkers.icon({
         markerColor: icon_colors[13],
@@ -222,9 +258,6 @@ d3.json(url_events).then(function(eventRes) {
         //Create variables to pull the information from the api (this is sample text for now. It depends on how we set up in the JSON)
         console.log("genres success")
 
-        /* ----------------------------------------------------------------------
-        NEW CHANGES: The joins below were why we were only getting Alternative genres. Just copy and paste these new ones and they should work.
-        ---------------------------------------------------------------------- */
         //Joining the tables "events", "venues"
         var joinedRes = eventRes.map(event => {
             const venue = venueRes.find(venue => venue.venue_id === event.venue_id);
@@ -334,7 +367,7 @@ d3.json(url_events).then(function(eventRes) {
                 //GenreCode = doubleJoinedRes[i].genre_name
         
                 /* -------------------------------------------------------
-                Create the marker for i
+                Create the marker for doubleJoinedRes[i]
                 ------------------------------------------------------- */
                 // Update the event count.
                 eventCount[GenreCode]++;
@@ -342,15 +375,12 @@ d3.json(url_events).then(function(eventRes) {
                 let newMarker = L.marker([doubleJoinedRes[i].latitude, doubleJoinedRes[i].longtitude], {
                 icon: icons[GenreCode]
                 });
-
                 // Add the new marker to the appropriate layer.
                 newMarker.addTo(layers[GenreCode]);
 
-                /*-------------------------------------------------------
-                NEW CHANGES: Everything below this is new: 
-                1st: markerData saves the current marker data into the options of newMarker
-                2nd: The popup now works. It is fairly messy but it should show [event_name, event_url, venue_name, event_date]
-                -------------------------------------------------------*/
+                //NEW CHANGES: newMarker gets added to the oms layer.
+                oms.addMarker(newMarker);
+
                 // Save info about the marker within itself so it can be pulled to the console when clicked on (Here is where you would want to decide what gets sent to the other dashboard objects).
                 let markerData = doubleJoinedRes[i]
                 newMarker.options.markerData = markerData;
@@ -359,7 +389,7 @@ d3.json(url_events).then(function(eventRes) {
                 newMarker.bindPopup("<b>" + doubleJoinedRes[i].event_name + "</b><br> Event: <a href =" + doubleJoinedRes[i].event_url + " target=_blank" + ">" + "More info </a><br>Venue: " + doubleJoinedRes[i].venue_name  + "<br> Event Date: " + doubleJoinedRes[i].event_date + " <br> ");
                 
                 /*###################################################
-                NEW CHANGES: IMPORTANT FOR SENDING INFO ABOUT THE SELECTED MARKER TO THE OTHER OBJECTS IN THE DASHBOARD
+                IMPORTANT FOR SENDING INFO ABOUT THE SELECTED MARKER TO THE OTHER OBJECTS IN THE DASHBOARD
                 ###################################################*/
                 // on click we access the stored info about the marker.
                 newMarker.on('click', function (info) {
@@ -371,17 +401,12 @@ d3.json(url_events).then(function(eventRes) {
                     
                   });
         }
-        
-        // NEW CHANGES: For the legend to work we update the count everytime
         // Call the updateLegend function, which will update the legend! 
         updateLegend(eventCount);
     });
   });
 });
 
-/*---------------------------------------------------------------
-NEW CHANGES: This function will create the html for the legend to the map!!
----------------------------------------------------------------*/
 // Update the legend's innerHTML with the last updated time and station count.
 function updateLegend(eventCount) {
     document.querySelector(".legend").innerHTML = [
